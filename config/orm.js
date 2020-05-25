3b6f139ace02e05017468ce93c29b5fa5e930391
Andrew Murray <atmurray@bellsouth.net>
2020-05-25 19:24:27 -0400

    created burger.js for burgers_db specific burger model

--- config/orm.js
+++ config/orm.js
@@ -3,34 +3,35 @@ const connection = require('./connection.js');
 
 
 var orm = {
-    selectAll:function(tableName){
+    selectAll:function(tableName,cBack){
         let qString = 'SELECT* FROM '+tableName
         connection.query(qString,function(err,result){
             if (err) throw err;
-            console.log(result);
+            cBack(result)
         })
     },
     
-    insertOne:function(tableName,colName1,colName2,burgerName,eaten){
+    insertOne:function(tableName,colName1,colName2,burgerName,eaten,cBack){
         let qString = 'INSERT INTO '+tableName+'('+colName1+', '+colName2+') VALUES (?,?)';
        
         connection.query(qString,[burgerName,eaten],function(err,result){
             if (err) throw err;
-            console.log(result);
+            cBack(result);
         })
     },
     
-    updateOne: function(tableName,colName1,colName2,burgerName,eaten){
-        console.log(tableName,colName1,colName2,burgerName,eaten);
+    updateOne: function(tableName,colName1,colName2,burgerName,eaten,cBack){
         let qString = 'UPDATE '+tableName+' SET '+colName1+'='+eaten+' WHERE ??=?';
         connection.query(qString,[colName2,burgerName],function(err,results){
-            if(err) throw err
-            console.log(results);
+            if(err){ throw err}
+            cBack(results);
         })
-        console.log()
+       
     }
 }
 // orm.insertOne('burgers','burger_name','devoured','MachoAndrew Burger',false);
-// orm.updateOne('burgers','devoured','burger_name','KarateKidBurger',false);
-orm.selectAll('burgers');
+// orm.updateOne('burgers','devoured','burger_name','whopper',true,function(res){
+//     console.log(res)
+// });
+// orm.selectAll('burgers');
 module.exports = orm;
--- models.js/burger.js
+++ models.js/burger.js
@@ -1,9 +1,34 @@
 const orm = require('../config/orm.js');
 
 var burger = {
-    all: function(cBack){
-        orm.selectAll("burgers",function(res){
-            cBack(res);
+    allBurgers: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        });
+
+    },
+    createBurger: function (burgerName, cb) {
+        orm.insertOne('burgers', 'burger_name', 'devoured', burgerName, false, function (res) {
+            
+            cb(res);
+        })
+    },
+    updateBurger: function (burgerName, cb) {
+        orm.updateOne('burgers', 'devoured', 'burger_name', burgerName, true, function (res) {
+            cb(res)
         })
     }
+
 }
+
+// burger.allBurgers(function(res){
+//     console.log(res);
+// })
+// burger.updateBurger('MachoAndrew Burger', function (result) {
+//     console.log(result)
+// })
+// burger.createBurger("The New NewBurger",(res)=>{
+//     console.log(res);
+// })
+
+module.exports = burger;
